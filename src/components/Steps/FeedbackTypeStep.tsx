13e219a17358f3b64cf6f97239b3b8180e916e51
unknown <francinesoares22@gmail.com>
2022-05-11 01:02:10 -0300

    create content step

--- src/components/Steps/FeedbackTypeStep.tsx
+++ src/components/Steps/FeedbackTypeStep.tsx
@@ -1,20 +1,39 @@
-import { feedbackTypes } from "../WidgetForm"
+import { CloseButton } from "../CloseButton";
+import { FeedbackType, feedbackTypes } from "../WidgetForm"
 
-export function FeedbackTypeStep() {
+interface feedbackTypeStepsProps {
+    onFeedbackTypeChanged: (type: FeedbackType) => void;
+}
+
+export function FeedbackTypeStep({ onFeedbackTypeChanged }: feedbackTypeStepsProps) {
+
     return (
-    <div className="flex py-8 gap-2 w-full">
-    { Object.entries(feedbackTypes).map(([key, value]) => { 
-        return (
-            <button 
-                key={ key } 
-                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent hover:brand-500 focus:border-brand-500 focus:outline-none" 
-                type="button"
-                onClick={() => setFeedbackType(key as FeedbackType)}
-                >
-                <img src={ value.image.source} alt={ value.image.alt } />
-                <span>{ value.title }</span>
-            </button>
-        )
-    }) }
-</div>)
+        <>
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
+
+                <CloseButton />
+            </header>
+
+            <div className="flex py-8 gap-2 w-full">
+                {Object.entries(feedbackTypes).map(([key, value]) => {
+                    return (
+
+                        <>
+                            <button
+                                key={key}
+                                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent hover:brand-500 focus:border-brand-500 focus:outline-none"
+                                type="button"
+                                onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
+                            >
+                                <img src={value.image.source} alt={value.image.alt} />
+                                <span>{value.title}</span>
+                            </button>
+                        </>
+                    )
+                })}
+            </div>
+        </>
+    )
+
 }
